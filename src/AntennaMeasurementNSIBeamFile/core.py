c709d2b01dbc7807afe1f8c7a17a40f3055303e0
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2026-09-18 14:07:33 +0300

    Add numpy dependency and implement pattern cut export functionality with validation in AntennaMeasurementNSIBeamFile class; enhance tests for initialization and export

--- src/AntennaMeasurementNSIBeamFile/core.py
+++ src/AntennaMeasurementNSIBeamFile/core.py
@@ -1,6 +1,7 @@
 
 import glob
 import os
+import numpy as np
 
 class AntennaMeasurementNSIBeamFile:
   
@@ -18,9 +19,10 @@ class AntennaMeasurementNSIBeamFile:
     self.folder = folder
     
     # Parse data from beam files
-    self.frequencies = []
+    self.frequencies  = []
+    self.axes         = []
     self.skipped_rows = 0
-    self.max_rows = 0
+    self.max_rows     = 0
     
     for beam_file in self.beam_files:
       with open(beam_file) as file:
@@ -30,7 +32,7 @@ class AntennaMeasurementNSIBeamFile:
             self.frequencies.append(float(lines[i+2].split()[1]))
             break
     
-    with open(beam_files[0]) as file:
+    with open(self.beam_files[0]) as file:
       lines = file.readlines()
       for i in range(len(lines)):
         if "P-pol Data" in lines[i]:
@@ -42,7 +44,56 @@ class AntennaMeasurementNSIBeamFile:
           self.max_rows = i+1
           break
 
-    self.max_rows -= self.skipped_rows+2
+      self.max_rows -= self.skipped_rows+2
+      self.axes = [lines[self.skipped_rows-1].split()[0].replace("(deg)", "").strip(), lines[self.skipped_rows-1].split()[1].replace("(deg)", "").strip()]
+    
+    
+  # Export pattern cut data 
+  def export_pattern_cut(self, constant_axis: str, constant_axis_value: float, frequency: float = 0.0) -> None:
+    # Check if frequency is specified and exists in the list of frequencies
+    if frequency != 0.0 and frequency not in self.frequencies:
+        raise ValueError(f"Frequency '{frequency}' not found in the list of frequencies")
+      
+    # Check constant axis allow lowercase and uppercase versions
+    if constant_axis.lower() not in [axis.lower() for axis in self.axes] or constant_axis.upper() not in [axis.upper() for axis in self.axes]:
+        raise ValueError(f"Invalid constant_axis '{constant_axis}'")
+    constant_axis_index = [axis.lower() for axis in self.axes].index(constant_axis.lower())
+
+    # If frequency is not specified iterate through all beam files
+    for i, beam_file in enumerate(self.beam_files):
+      hangle, vangle, co_amp, co_phase = np.loadtxt(beam_file, skiprows=self.skipped_rows, max_rows=self.max_rows, unpack=True)
+      hangle, vangle, cr_amp, cr_phase = np.loadtxt(beam_file, skiprows=self.skipped_rows+self.max_rows+2, max_rows=self.max_rows, unpack=True)
+      
+      axis_angles = [hangle, vangle]
+      
+      # Checkf if the constant axis value exists in the axis angles
+      if constant_axis_value not in axis_angles[constant_axis_index]:
+          raise ValueError(f"Constant axis value '{constant_axis_value}' not found in the axis angles")
+        
+      # Extract the cut data for the specified constant axis value
+      sweep_angle  = []
+      co_amp_cut   = []
+      co_phase_cut = []
+      cr_amp_cut   = []
+      cr_phase_cut = []
+      for j in range(len(axis_angles[constant_axis_index])):
+        if axis_angles[constant_axis_index][j] == constant_axis_value:
+          sweep_angle.append(axis_angles[1-constant_axis_index][j])
+          co_amp_cut.append(co_amp[j])
+          co_phase_cut.append(co_phase[j])
+          cr_amp_cut.append(cr_amp[j])
+          cr_phase_cut.append(cr_phase[j])
+      
+ 
+      # Create "Cuts" folder in the same directory as the beam file if it doesn't exist
+      cuts_folder = os.path.join(os.path.dirname(beam_file), "Cut/Data")
+      if not os.path.exists(cuts_folder):
+          os.makedirs(cuts_folder)
+          
+      # Save the pattern cut data to a file in the "Cuts" folder
+      cut_file = os.path.join(cuts_folder, f"{self.frequencies[i]:.3f}GHz_{constant_axis}={constant_axis_value:.03f}deg.csv")
+      np.savetxt(cut_file, np.column_stack((sweep_angle, co_amp_cut, co_phase_cut, cr_amp_cut, cr_phase_cut)), header="Angle[deg], Co_Amp[dB], Co_Phase[deg], Cross_Amp[dB], Cross_Phase[deg]", fmt='%.3f, %.6f, %.3f, %.6f, %.3f')
+
 
 
 def hello() -> str:
--- tests/test_core.py
+++ tests/test_core.py
@@ -13,5 +13,22 @@ def test_antenna_measurement_nsi_beam_file_init():
   # Initialize the class
   antenna_measurement = AntennaMeasurementNSIBeamFile(sample_folder)
 
-  print(antenna_measurement.frequencies, antenna_measurement.skipped_rows, antenna_measurement.max_rows)
+  print(antenna_measurement.frequencies, antenna_measurement.skipped_rows, antenna_measurement.max_rows, antenna_measurement.axes)
   assert 0 == 0
+
+def test_export_pattern_cut():
+  from AntennaMeasurementNSIBeamFile.core import AntennaMeasurementNSIBeamFile
+
+  sample_folder = "sample_data" 
+
+  # Initialize the class
+  antenna_measurement = AntennaMeasurementNSIBeamFile(sample_folder)
+
+  # Export a pattern cut for testing
+  constant_axis = "theta"
+  constant_axis_value = 0.0
+  frequency = antenna_measurement.frequencies[0]
+  antenna_measurement.export_pattern_cut(constant_axis, constant_axis_value, frequency)
+
+  assert 0 == 0
+  
